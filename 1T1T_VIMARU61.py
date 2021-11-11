e4fd2df87a67389c551058f2f8e9bdbb86e87924
tdbui1209 <tdbui1209@gmail.com>
2021-11-11 12:55:07 +0700

    Add files via upload

---
+++ 1T1T_VIMARU61.py
@@ -0,0 +1,40 @@
+# ==============================================================================
+# Cau 1:
+
+from tensorflow import keras
+from tensorflow.keras import layers
+
+
+def build_linear_regression_model(input_dim):
+    '''
+    Input:
+        input_dim: Number of features
+
+    Output:
+        Linear Regression predict model
+    '''
+    model = keras.Sequential([
+        layers.Dense(units=1, activation='relu', input_shape=input_dim),
+    ])
+    return model
+
+# ==============================================================================
+# Cau 2:
+
+from sklearn.neighbors import KNeighborsClassifier
+
+
+def train(X_train, y_train, X_test, k):
+    '''
+    Input: 
+        X_train: Training data
+        y_train: Training target
+        X_test: Test data
+        k: Number of neighbors to use
+        
+    Output:
+        y_test: Predicted target of X_test
+    '''
+    model = KNeighborsClassifier(n_neighbors=k, metric='euclidean')
+    model.fit(X_train, y_train)
+    return model.predict(X_test)
