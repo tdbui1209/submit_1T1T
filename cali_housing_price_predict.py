a1ef2309efbddce18d03f9b7e26deb5110fa5fe9
tdbui1209 <tdbui1209@gmail.com>
2021-11-14 13:21:04 +0700

    add test linear regression

---
+++ cali_housing_price_predict.py
@@ -0,0 +1,21 @@
+from 1T1T_VIMARU61 import build_linear_regression_model
+from sklearn.datasets import fetch_california_housing
+from sklearn.metrics import mean_squared_error
+
+dataset = fetch_california_housing()
+X_train, X_test, y_train, y_test = train_test_split(dataset.data, dataset.target, test_size=0.2)
+
+model = build_linear_regression_model([X_train.shape[1]])
+
+print('X_train shape:', X_train.shape)
+print('y_train shape:', y_train.shape)
+print('X_test shape:', X_test.shape)
+
+model.compile(optimizer='adam', loss='mse')
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+
+print('Actual target:', y_test)
+print('Predicted target:', y_pred)
+
+print('MSE: {:2f}'.format(mean_squared_error(y_test, y_pred)))
