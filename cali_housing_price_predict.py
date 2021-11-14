a612425bf19d832149f161c1f07666d853a5bdca
tdbui1209 <tdbui1209@gmail.com>
2021-11-14 13:34:38 +0700

    fix import lib

--- cali_housing_price_predict.py
+++ cali_housing_price_predict.py
@@ -1,11 +1,12 @@
-from 1T1T_VIMARU61 import build_linear_regression_model
 from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
+my_lib = __import__('1T1T_VIMARU61')
 
 dataset = fetch_california_housing()
 X_train, X_test, y_train, y_test = train_test_split(dataset.data, dataset.target, test_size=0.2)
 
-model = build_linear_regression_model([X_train.shape[1]])
+model = my_lib.build_linear_regression_model([X_train.shape[1]])
 
 print('X_train shape:', X_train.shape)
 print('y_train shape:', y_train.shape)
@@ -15,7 +16,7 @@ model.compile(optimizer='adam', loss='mse')
 model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
 
-print('Actual target:', y_test)
-print('Predicted target:', y_pred)
+print('\nActual target:', y_test)
+print('\nPredicted target:', y_pred)
 
-print('MSE: {:2f}'.format(mean_squared_error(y_test, y_pred)))
+print('\nMSE: {:2f}'.format(mean_squared_error(y_test, y_pred)))
--- iris_predict.py
+++ iris_predict.py
@@ -1,7 +1,7 @@
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-from 1T1T_VIMARU61 import knn_train_pred
+my_lib = __import__('1T1T_VIMARU61')
 
 dataset = load_iris()
 X_train, X_test, y_train, y_test = train_test_split(dataset.data, dataset.target, test_size=0.2)
@@ -10,12 +10,12 @@ print('X_train shape:', X_train.shape)
 print('y_train shape:', y_train.shape)
 print('X_test shape:', X_test.shape)
 
-y_pred = knn_train_pred(X_train, y_train, X_test, 5)
+y_pred = my_lib.knn_train_pred(X_train, y_train, X_test, 5)
 
 y_actual = [dataset.target_names[i] for i in y_test]
-print('Actual target:', y_actual)
+print('\nActual target:', y_actual)
 
 y_pred = [dataset.target_names[i] for i in y_pred]
-print('Predicted target:', y_pred)
+print('\nPredicted target:', y_pred)
 
-print('Accuracy score: {:.2f}'.format(accuracy_score(y_test, y_pred)))
+print('\nAccuracy score: {:.2f}'.format(accuracy_score(y_actual, y_pred)))
